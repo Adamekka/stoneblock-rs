5b4b0ff2f2e9ebed696e2bf21e25f5101e4cecb5
Adam Cvikl <adam.wasd4@gmail.com>
2023-05-13 21:59:52 +0200

    make triangle move

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,10 @@
-use glium::{glutin, implement_vertex, uniform, Surface};
+use glium::{glutin, implement_vertex, index::NoIndices, uniform, Surface, VertexBuffer};
+
+#[derive(Copy, Clone)]
+struct Vertex {
+    position: [f32; 2],
+    color: [f32; 3],
+}
 
 fn main() {
     let event_loop = glutin::event_loop::EventLoop::new();
@@ -7,95 +13,86 @@ fn main() {
     let display = glium::Display::new(window_builder, context_builder, &event_loop)
         .expect("Failed to create display");
 
-    // Build the vertex buffer
-    let vertex_buffer = {
-        #[derive(Copy, Clone)]
-        struct Vertex {
-            position: [f32; 2],
-            color: [f32; 3],
-        }
+    let shape: [Vertex; 3] = [
+        Vertex {
+            position: [-0.5, -0.5],
+            color: [0.0, 1.0, 0.0],
+        },
+        Vertex {
+            position: [0.5, -0.5],
+            color: [0.0, 0.0, 1.0],
+        },
+        Vertex {
+            position: [0.0, 0.5],
+            color: [1.0, 0.0, 0.0],
+        },
+    ];
 
-        implement_vertex!(Vertex, position, color);
+    implement_vertex!(Vertex, position, color);
 
-        glium::VertexBuffer::new(
-            &display,
-            &[
-                Vertex {
-                    position: [-0.5, -0.5],
-                    color: [0.0, 1.0, 0.0],
-                },
-                Vertex {
-                    position: [0.5, -0.5],
-                    color: [0.0, 0.0, 1.0],
-                },
-                Vertex {
-                    position: [0.0, 0.5],
-                    color: [1.0, 0.0, 0.0],
-                },
-            ],
-        )
-        .unwrap()
-    };
+    let vertex_buffer = glium::VertexBuffer::new(&display, &shape).unwrap();
 
     // Build the index buffer
-    let index_buffer = glium::IndexBuffer::new(
-        &display,
-        glium::index::PrimitiveType::TrianglesList,
-        &[0u16, 1, 2],
-    )
-    .unwrap();
+    let indices = glium::index::NoIndices(glium::index::PrimitiveType::TrianglesList);
 
     // Compile shaders and link them together
-    let triangle_vertex = std::fs::read_to_string("src/shaders/triangle_vertex.glsl")
+    let triangle_vertex = std::fs::read_to_string("src/shaders/triangle.vert")
         .expect("Failed to read triangle_vertex.glsl");
-    let triangle_fragment = std::fs::read_to_string("src/shaders/triangle_fragment.glsl")
+    let triangle_fragment = std::fs::read_to_string("src/shaders/triangle.frag")
         .expect("Failed to read triangle_fragment.glsl");
 
     let program =
         glium::Program::from_source(&display, &triangle_vertex, &triangle_fragment, None).unwrap();
 
-    // Draw black background and triangle
-    let draw = move || {
-        // Build the uniforms
-        let uniforms = uniform! {
-            matrix: [
-                [1.0,0.0,0.0,0.0],
-                [0.0,1.0,0.0,0.0],
-                [0.0,0.0,1.0,0.0],
-                [0.0,0.0,0.0,1.0f32],
-            ]
-        };
-
-        // Draw a frame
-        let mut frame = display.draw();
-        frame.clear_color(0.0, 0.0, 0.0, 0.0);
-        frame
-            .draw(
-                &vertex_buffer,
-                &index_buffer,
-                &program,
-                &uniforms,
-                &Default::default(),
-            )
-            .unwrap();
-        frame.finish().unwrap();
-    };
+    let mut step: f32 = -0.5;
 
     // Draw the triangle
-    draw();
+    draw(&display, &vertex_buffer, &indices, &program, step);
 
     event_loop.run(move |event, _, control_flow| {
         *control_flow = match event {
             glutin::event::Event::WindowEvent { event, .. } => match event {
                 // Close the window if the escape key or the exit button is pressed.
                 glutin::event::WindowEvent::CloseRequested => glutin::event_loop::ControlFlow::Exit,
-                glutin::event::WindowEvent::Resized(..) => {
-                    draw();
-                    glutin::event_loop::ControlFlow::Poll
-                }
+                glutin::event::WindowEvent::Resized(..) => glutin::event_loop::ControlFlow::Poll,
                 _ => glutin::event_loop::ControlFlow::Poll,
             },
             _ => glutin::event_loop::ControlFlow::Poll,
         };
+
+        // Wait until the next frame
+        let next_frame_time =
+            std::time::Instant::now() + std::time::Duration::from_nanos(16_666_667);
+        *control_flow = glutin::event_loop::ControlFlow::WaitUntil(next_frame_time);
+
+        step += 0.0002;
+        if step > 0.5 {
+            step = -0.5;
+        }
+
+        draw(&display, &vertex_buffer, &indices, &program, step);
     });
+}
+
+/// Draw a frame\
+/// Draw black background and triangle
+fn draw(
+    display: &glium::Display,
+    vertex_buffer: &VertexBuffer<Vertex>,
+    index_buffer: &NoIndices,
+    program: &glium::Program,
+    step: f32,
+) {
+    let mut frame = display.draw();
+    frame.clear_color(0.0, 0.0, 0.0, 0.0);
+    frame
+        .draw(
+            vertex_buffer,
+            index_buffer,
+            program,
+            &uniform! { step: step },
+            &Default::default(),
+        )
+        .unwrap();
+    frame.finish().unwrap();
 }
--- src/shaders/triangle.vert
+++ src/shaders/triangle.vert
@@ -5,7 +5,11 @@ in vec3 color;
 
 out vec3 vColor;
 
+uniform float step;
+
 void main() {
-  gl_Position = vec4(position, 0.0, 1.0);
+  vec2 pos = position;
+  pos.x += step;
+  gl_Position = vec4(pos, 0.0, 1.0);
   vColor = color;
 }
